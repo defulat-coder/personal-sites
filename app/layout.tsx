87946511fe5bfa4b21b40995d0b1419691e62192
yuan.chen <yuan.chen@xibojiaoyu.com>
2026-08-09 15:16:22 +0800

    chore: 更名每日关注

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,12 +6,12 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   description:
-    "陈远的个人网站：工作履历、工程实践与每日策展。",
+    "陈远的个人网站：工作履历、工程实践与每日关注。",
   robots: {
     follow: false,
     index: false,
   },
-  title: "陈远｜每日策展",
+  title: "陈远｜每日关注",
 };
 
 export default function RootLayout({
